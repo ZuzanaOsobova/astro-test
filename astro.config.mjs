67f2566a4e3663ff39bd64730ba907e8818fb5e3
Zuzana Osobová <zuzana.osobova@gmail.com>
2026-04-15 15:52:06 +0200

    Unit 1 a 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://zuzanaosobova.github.io',
-    base: '/astro-test/',
 });
+//github pages nespolupracují s link systémem a místo indexu ukazují readme, prostě na to zatím kašlu, tohle vymyslím až bych někdy reálně publikovala webovky
