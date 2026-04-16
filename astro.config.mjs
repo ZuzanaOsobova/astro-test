d501d2a25d4c53a382ef562d71a1fe21d3336f45
Zuzana Osobová <zuzana.osobova@gmail.com>
2026-04-16 14:56:31 +0200

    Unit 5 a 6

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from '@astrojs/preact';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://zuzanaosobova.github.io',
+  site: 'https://zuzanaosobova.github.io',
+  integrations: [preact()],
 });
 //github pages nespolupracují s link systémem a místo indexu ukazují readme, prostě na to zatím kašlu, tohle vymyslím až bych někdy reálně publikovala webovky
