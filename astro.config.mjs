2abead40c546b6528f1d52cf1ee47ab21d7f71cb
Zuzana Osobová <zuzana.osobova@gmail.com>
2026-04-20 09:35:17 +0200

    small github changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,9 @@ import { defineConfig } from 'astro/config';
 
 import preact from '@astrojs/preact';
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://zuzanaosobova.github.io',
+  base: 'astro-test',
   integrations: [preact()],
 });
 //github pages nespolupracují s link systémem a místo indexu ukazují readme, prostě na to zatím kašlu, tohle vymyslím až bych někdy reálně publikovala webovky
